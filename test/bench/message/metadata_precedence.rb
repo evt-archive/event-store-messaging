ad751d5e1fec631a957308a89b45443749d51bdd
Scott Bellware <sbellware@gmail.com>
2016-07-07 16:50:01 -0500

    Setup is removed from the test block

--- test/bench/message/metadata_precedence.rb
+++ test/bench/message/metadata_precedence.rb
@@ -5,11 +5,11 @@ context "Metadata Precedence" do
   metadata = EventStore::Messaging::Message::Metadata.new
 
   context "Relevant attributes are equal" do
-    test "Message metadata has precedence" do
-      metadata.causation_event_uri = source_metadata.source_event_uri
-      metadata.correlation_stream_name = source_metadata.correlation_stream_name
-      metadata.reply_stream_name = source_metadata.reply_stream_name
+    metadata.causation_event_uri = source_metadata.source_event_uri
+    metadata.correlation_stream_name = source_metadata.correlation_stream_name
+    metadata.reply_stream_name = source_metadata.reply_stream_name
 
+    test "Message metadata has precedence" do
       assert(metadata.precedence?(source_metadata))
     end
   end
@@ -17,13 +17,13 @@ context "Metadata Precedence" do
   context "Any of the relevant attributes are not equal" do
     [:causation_event_uri, :correlation_stream_name, :reply_stream_name].each do |attribute|
 
+      metadata.causation_event_uri = source_metadata.source_event_uri
+      metadata.correlation_stream_name = source_metadata.correlation_stream_name
+      metadata.reply_stream_name = source_metadata.reply_stream_name
+
+      metadata.send "#{attribute}=", SecureRandom.hex
+
       test attribute.to_s do
-        metadata.causation_event_uri = source_metadata.source_event_uri
-        metadata.correlation_stream_name = source_metadata.correlation_stream_name
-        metadata.reply_stream_name = source_metadata.reply_stream_name
-
-        metadata.send "#{attribute}=", SecureRandom.hex
-
         assert(!metadata.precedence?(source_metadata))
       end
     end
